89173622b7b3ab215ef06204ee68020c7796b33a
yamano <hikaru32699@gmail.com>
2021-12-11 11:26:36 +0900

    初回コミット

---
+++ ARC029_A.py
@@ -0,0 +1,15 @@
+n = int(input())
+t = [int(input()) for _ in range(n)]
+result = -1
+for i in range(2**n):
+  a = 0
+  b = 0  
+  for j in range(n):
+    if ((i >> j) & 1):
+      a += t[j] 
+    else:
+      b += t[j]
+  
+  if result == -1 or result > max(a, b):
+    result = max(a, b)
+print(result)
